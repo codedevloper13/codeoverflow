8e23ff20a6868105ce6a9bf48f95f235ddffe96a
codedevloper13 <codedevloper13@gmail.com>
2023-11-06 22:07:56 +0530

    07_Authentication:Cleark authenticationAdded

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,10 @@
+/** @format */
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+	return (
+		<div>
+			<UserButton afterSignOutUrl='/' />
+		</div>
+	);
+}
---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from "react";
+
+const page = () => {
+	return <div>Ask Question</div>;
+};
+
+export default page;
